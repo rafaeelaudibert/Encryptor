dcd2cc1d9342889d0760f30e2c056a810bbf8833
RafaAudibert <rafaeelaudibert@gmail.com>
2019-09-26 12:26:34 -0300

    :green_heart: Fixed tests build

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,21 +7,9 @@ from encryptor import create_app
 
 
 @pytest.fixture
-def app():
-    """Create and configure a new app instance for each test."""
+def client():
+    """Create and configure a new app instance for each test, with a test client for the app."""
     # create the app with common test config
     app = create_app({"TESTING": True})
 
-    yield app
-
-
-@pytest.fixture
-def client(app):
-    """A test client for the app."""
     return app.test_client()
-
-
-@pytest.fixture
-def runner(app):
-    """A test runner for the app's Click commands."""
-    return app.test_cli_runner()
