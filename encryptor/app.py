b35770be141d9927cb76c1fd630833e4027cc86a
RafaAudibert <rafaeelaudibert@gmail.com>
2019-09-27 22:37:04 -0300

    :fire: Remove unused file

--- encryptor/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-
-# Initialize Flask app
-app = Flask(__name__)
-
-# ROUTING
-@app.route('/api/', methods=['GET'])
-def root():
-    return jsonify({'content': 'Hello World!'})
