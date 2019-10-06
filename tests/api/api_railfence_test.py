e972f6646b170484e9d7049e9d71df8c9b35b262
sigurdurhelga <sigurdurhel@ru.is>
2019-10-06 14:23:47 +0000

    :white_check_mark: fixed names in test + added bad railheight calls

--- tests/api/api_railfence_test.py
+++ tests/api/api_railfence_test.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 def test_railfence_encrypt_api(client):
     response = client.get(
         "/api/railfence/encrypt/helloworld?rail_height=2").get_json()
@@ -8,10 +7,14 @@ def test_railfence_encrypt_api(client):
     assert response["content"] == "hloolelwrd"
 
 
-def test_railfence_encrypt_api_no_offset(client):
+def test_railfence_encrypt_api_no_rail_height(client):
     response = client.get("/api/railfence/encrypt/helloworld").get_json()
     assert response["status"] == 200
     assert response["content"] == "holelwrdlo"
+
+def test_railfence_encrypt_api_invalid_rail_height(client):
+    response = client.get("/api/railfence/encrypt/helloworld?rail_height=two").get_json()
+    assert response["status"] == 400
 
 
 def test_railfence_decrypt_api(client):
@@ -21,7 +24,11 @@ def test_railfence_decrypt_api(client):
     assert response["content"] == "helloworld"
 
 
-def test_ceasar_decrypt_api_no_offset(client):
+def test_railfence_decrypt_api_no_rail_height(client):
     response = client.get("/api/railfence/decrypt/holelwrdlo").get_json()
     assert response["status"] == 200
     assert response["content"] == "helloworld"
+
+def test_railfence_decrypt_api_invalid_rail_height(client):
+    response = client.get("/api/railfence/decrypt/helloworld?rail_height=two").get_json()
+    assert response["status"] == 400
