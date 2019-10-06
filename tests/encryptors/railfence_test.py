b3de09474c8fbd3f65b9d02d171ba1daa34d678d
sigurdurhelga <sigurdurhel@ru.is>
2019-10-06 12:32:42 +0000

    added some more tests for railfence

--- tests/encryptors/railfence_test.py
+++ tests/encryptors/railfence_test.py
@@ -10,4 +10,10 @@ def test_encrypt():
 
     """
     assert RailFence.encrypt("WE ARE DISCOVERED. FLEE AT ONCE", 3) == "WECRLTEERDSOEEFEAOCAIVDEN"
+    assert RailFence.encrypt("I REALLY LIKE PUZZLES", 3) == "ILIUERALLKPZLSEYEZ"
+    assert RailFence.encrypt("RAILFENCEENCRYPTOR", 2) == "RIFNENRPOALECECYTR"
+    assert RailFence.encrypt("RAIL", 5) == "RAIL"
+    assert RailFence.encrypt("R", 5) == "R"
+    assert RailFence.encrypt("R", 1) == "R"
+    assert RailFence.encrypt("R", 100) == "R"
 
