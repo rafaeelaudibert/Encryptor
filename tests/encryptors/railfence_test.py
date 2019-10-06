f683d675da4e81a46ad780de9ea1c355829d103d
sigurdurhelga <sigurdurhel@ru.is>
2019-10-06 12:59:24 +0000

    Tests for decryption + tests on encryption for spaces

--- tests/encryptors/railfence_test.py
+++ tests/encryptors/railfence_test.py
@@ -9,11 +9,20 @@ def test_encrypt():
     """
 
     """
-    assert RailFence.encrypt("WE ARE DISCOVERED. FLEE AT ONCE", 3) == "WECRLTEERDSOEEFEAOCAIVDEN"
-    assert RailFence.encrypt("I REALLY LIKE PUZZLES", 3) == "ILIUERALLKPZLSEYEZ"
+    assert RailFence.encrypt("WE ARE DISCOVERED FLEE AT ONCE", 3) == "WRIVDETCEAEDSOEE LEA NE  CRF O"
+    assert RailFence.encrypt("I REALLY LIKE PUZZLES", 3) == "IA EZS ELYLK UZERLIPL"
     assert RailFence.encrypt("RAILFENCEENCRYPTOR", 2) == "RIFNENRPOALECECYTR"
     assert RailFence.encrypt("RAIL", 5) == "RAIL"
     assert RailFence.encrypt("R", 5) == "R"
     assert RailFence.encrypt("R", 1) == "R"
     assert RailFence.encrypt("R", 100) == "R"
+
+def test_decrypt():
+    assert RailFence.decrypt("WRIVDETCEAEDSOEE LEA NE  CRF O",3) == "WE ARE DISCOVERED FLEE AT ONCE"
+    assert RailFence.decrypt("IA EZS ELYLK UZERLIPL",3) == "I REALLY LIKE PUZZLES"
+    assert RailFence.decrypt("IAM",3) == "IAM"
+    assert RailFence.decrypt("IAM",100) == "IAM"
+    assert RailFence.decrypt("I",1) == "I"
+    assert RailFence.decrypt("IM",1) == "IM"
+
 
