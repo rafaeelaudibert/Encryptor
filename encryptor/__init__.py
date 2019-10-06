7db8449d60151358b0ce07acd1d15c68ff608f18
Restyled.io <commits@restyled.io>
2019-10-06 04:45:47 +0000

    :recycle: Restyled by black

--- encryptor/__init__.py
+++ encryptor/__init__.py
@@ -12,15 +12,18 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 
 # Dotenv
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
 # Sentry initialization
 sentry_sdk.init(
-    dsn=os.environ['SENTRY_DSN'] or os.getenv('SENTRY_DSN'),
+    dsn=os.environ["SENTRY_DSN"] or os.getenv("SENTRY_DSN"),
     release="encryptor@{}".format(
-        os.environ['PACKAGE_VERSION'] or os.getenv('PACKAGE_VERSION')) or '0.0.0',
-    integrations=[FlaskIntegration()]
+        os.environ["PACKAGE_VERSION"] or os.getenv("PACKAGE_VERSION")
+    )
+    or "0.0.0",
+    integrations=[FlaskIntegration()],
 )
 
 
@@ -50,24 +53,24 @@ def create_app(test_config=None):
     # ROUTES
     @app.route("/api/ceasar/encrypt/<text>")
     def ceasar_encrypt(text):
-        offset = request.args.get('offset', Ceasar.DEFAULT_OFFSET)
+        offset = request.args.get("offset", Ceasar.DEFAULT_OFFSET)
 
-        return jsonify({'status': 200, 'content': Ceasar.encrypt(text, offset)})
+        return jsonify({"status": 200, "content": Ceasar.encrypt(text, offset)})
 
     @app.route("/api/ceasar/decrypt/<text>")
     def ceasar_decrypt(text):
-        offset = request.args.get('offset', Ceasar.DEFAULT_OFFSET)
+        offset = request.args.get("offset", Ceasar.DEFAULT_OFFSET)
 
-        return jsonify({'status': 200, 'content': Ceasar.decrypt(text, offset)})
+        return jsonify({"status": 200, "content": Ceasar.decrypt(text, offset)})
 
-    @app.route('/api/error')
+    @app.route("/api/error")
     def trigger_error():
-        return jsonify({'status': 200, 'content': '1 / 0 = {}'.format(1 / 0)})
+        return jsonify({"status": 200, "content": "1 / 0 = {}".format(1 / 0)})
 
     # ERROR HANDLERS
     def handle_errors(e):
-        return jsonify(
-            {'status': e.code, 'error': e.name, 'content': e.description})
+        return jsonify({"status": e.code, "error": e.name, "content": e.description})
+
     app.register_error_handler(400, handle_errors)
     app.register_error_handler(500, handle_errors)
 
--- tests/api/api_ceasar_test.py
+++ tests/api/api_ceasar_test.py
@@ -2,54 +2,46 @@ import pytest
 
 
 def test_ceasar_encrypt_api(client):
-    response = client.get(
-        '/api/ceasar/encrypt/encryptthis?offset=2').get_json()
-    assert response['status'] == 200
-    assert response['content'] == 'gpetarvvjku'
+    response = client.get("/api/ceasar/encrypt/encryptthis?offset=2").get_json()
+    assert response["status"] == 200
+    assert response["content"] == "gpetarvvjku"
 
 
 def test_ceasar_encrypt_api_no_offset(client):
-    response = client.get(
-        '/api/ceasar/encrypt/encryptthis').get_json()
-    assert response['status'] == 200
-    assert response['content'] == 'encryptthis'
+    response = client.get("/api/ceasar/encrypt/encryptthis").get_json()
+    assert response["status"] == 200
+    assert response["content"] == "encryptthis"
 
 
 def test_ceasar_encrypt_invalid_api(client):
-    response = client.get(
-        '/api/ceasar/encrypt/encryptthis00?offset=2').get_json()
-    assert response['status'] == 400
+    response = client.get("/api/ceasar/encrypt/encryptthis00?offset=2").get_json()
+    assert response["status"] == 400
 
 
 def test_ceasar_decrypt_api(client):
-    response = client.get(
-        '/api/ceasar/decrypt/fgetarvvjku?offset=2').get_json()
-    assert response['status'] == 200
-    assert response['content'] == 'decryptthis'
+    response = client.get("/api/ceasar/decrypt/fgetarvvjku?offset=2").get_json()
+    assert response["status"] == 200
+    assert response["content"] == "decryptthis"
 
 
 def test_ceasar_decrypt_api_no_offset(client):
-    response = client.get(
-        '/api/ceasar/decrypt/encryptthis').get_json()
-    assert response['status'] == 200
-    assert response['content'] == 'encryptthis'
+    response = client.get("/api/ceasar/decrypt/encryptthis").get_json()
+    assert response["status"] == 200
+    assert response["content"] == "encryptthis"
 
 
 def test_ceasar_decrypt_invalid_api(client):
-    response = client.get(
-        '/api/ceasar/decrypt/fgetarvvjku00?offset=2').get_json()
-    assert response['status'] == 400
+    response = client.get("/api/ceasar/decrypt/fgetarvvjku00?offset=2").get_json()
+    assert response["status"] == 400
 
 
 def test_ceasar_encrypt_invalid_offset(client):
-    response = client.get(
-        '/api/ceasar/encrypt/fgetarvvjku00?offset=abc').get_json()
-    assert response['status'] == 400
-    assert 'Should be <class \'int\'>' in response['content']
+    response = client.get("/api/ceasar/encrypt/fgetarvvjku00?offset=abc").get_json()
+    assert response["status"] == 400
+    assert "Should be <class 'int'>" in response["content"]
 
 
 def test_ceasar_decrypt_invalid_offset(client):
-    response = client.get(
-        '/api/ceasar/decrypt/fgetarvvjku00?offset=abc').get_json()
-    assert response['status'] == 400
-    assert 'Should be <class \'int\'>' in response['content']
+    response = client.get("/api/ceasar/decrypt/fgetarvvjku00?offset=abc").get_json()
+    assert response["status"] == 400
+    assert "Should be <class 'int'>" in response["content"]
