f758aa427f6e9c38fe2441efda8ce3b4865e7d28
RafaAudibert <rafaeelaudibert@gmail.com>
2019-10-08 16:59:53 -0300

    :bug: Fix bugs with Vigenere key

--- tests/encryptors/vigenere_test.py
+++ tests/encryptors/vigenere_test.py
@@ -4,42 +4,6 @@ from encryptor.encryptors import Vigenere
 from encryptor.errors import *
 
 
-def test_encrypt_letter():
-    """
-    Test basic encrypt letters function
-    """
-    assert Vigenere.encrypt("a", "q") == "R"
-    assert Vigenere.encrypt("j", "d") == "N"
-    assert Vigenere.encrypt("x", "r") == "j"
-    assert Vigenere.encrypt("t", "N") == "B"
-    assert Vigenere.encrypt("R", "v") == "H"
-
-
-def test_encrypt_letter_no_offset():
-    """
-    Test basic encrypt letters function omitting `key` parameter
-    """
-    assert Vigenere.encrypt("r") == "S"
-
-
-def test_decrypt_letter():
-    """
-    Test basic decrypt letters function with lower and upper case letters
-    """
-    assert Vigenere.decrypt("R", "q") == "a"
-    assert Vigenere.decrypt("N", "d") == "j"
-    assert Vigenere.decrypt("j", "r") == "x"
-    assert Vigenere.decrypt("B", "N") == "t"
-    assert Vigenere.decrypt("H", "v") == "R"
-
-
-def test_decrypt_letter_no_offset():
-    """
-    Test basic decrypt letters function omitting `key` parameter
-    """
-    assert Vigenere.decrypt("S") == "r"
-
-
 def test_encrypt():
     """
     Test basic encrypt text function, with lower and upper case characters
