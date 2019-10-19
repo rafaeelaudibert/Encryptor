b279b0284c21f8008e28958672804d19a3c0d453
Pragyan Mehrotra <pragyan18168@iiitd.ac.in>
2019-10-19 10:51:57 +0530

    Changed default parameters name to upper case, used ternary operators, created a custom exception and used that, Added spaces between function calls

--- encryptor/encryptors/rsa.py
+++ encryptor/encryptors/rsa.py
@@ -1,69 +1,57 @@
 from ..errors.not_allowed_value import NotAllowedValue
 from ..errors.wrong_type_parameter import WrongTypeParameter
+from ..errors.modular_inverse_does_not_exist import ModularInverseDoesNotExist
 from binascii import hexlify,unhexlify
 
 
 class Rsa:
 
-	Default_E = 65537
-	Default_P = 296364335885826735335603657657712346243
-	Default_Q = 261610179866133310725739923400935476237
-	Default_N = Default_P*Default_Q
+	DEFAULT_E = 65537
+	DEFAULT_P = 296364335885826735335603657657712346243
+	DEFAULT_Q = 261610179866133310725739923400935476237
+	DEFAULT_N = DEFAULT_P*DEFAULT_Q
 
 	@staticmethod
-	def encrypt(text: str, n: int = Default_N, e: int = Default_E):
-		if (n is None):
-			n = Default_N
-		else:
-			try:
-				n = int(n)
-			except ValueError:
-				raise WrongTypeParameter("n", int, type(n))
+	def encrypt(text: str, n: int = DEFAULT_N, e: int = DEFAULT_E):
+		try:
+			n = Rsa.DEFAULT_N if n is None else int(n)
+		except ValueError:
+			raise WrongTypeParameter("n", int, type(n))
 
-		if (e is None):
-			e = Rsa.Default_E
-		else:
-			try:
-				e = int(e)
-			except ValueError:
-				raise WrongTypeParameter("e", int, type(e))
+		try:
+			e = Rsa.DEFAULT_E if e is None else int(e)
+		except ValueError:
+			raise WrongTypeParameter("e", int, type(e))
 
-		message = int(hexlify(bytes(text,'utf-8')),16)
-		encrypted_message = pow(message,e,n)
+		message = int(hexlify(bytes(text, 'utf-8')), 16)
+		encrypted_message = pow(message, e, n)
 		return hex(encrypted_message)[2:].split('L')[0]
 
 	@staticmethod
-	def decrypt(text: str, p: int = Default_P, q: int = Default_Q, e: int = Default_E):
-		if (p is None):
-			p = Rsa.Default_P
-		else:
-			try:
-				p = int(p)
-			except ValueError:
-				raise WrongTypeParameter("p", int, type(p))
-
-		if (q is None):
-			q = Rsa.Default_Q
-		else:
-			try:
-				q = int(q)
-			except ValueError:
-				raise WrongTypeParameter("q", int, type(q))
-
-		if (e is None):
-			e = Rsa.Default_E
-		else:
-			try:
-				e = int(e)
-			except ValueError:
-				raise WrongTypeParameter("e", int, type(e))
+	def decrypt(text: str, p: int = DEFAULT_P, q: int = DEFAULT_Q, e: int = DEFAULT_E):
 		try:
-			encrypted_message = int(text,16)
+			p = Rsa.DEFAULT_P if p is None else int(p)
+		except ValueError:
+			raise WrongTypeParameter("p", int, type(p))
+		
+		try:
+			q = Rsa.DEFAULT_Q if q is None else int(q)
+		except ValueError:
+			raise WrongTypeParameter("q", int, type(q))
+
+		
+		try:
+			e = Rsa.DEFAULT_E if e is None else int(e)
+		except ValueError:
+			raise WrongTypeParameter("e", int, type(e))
+
+		try:
+			encrypted_message = int(text, 16)
 		except:
 			raise NotAllowedValue
 
 		phi = (p-1)*(q-1)
-		d = Rsa.modinv(e,phi)
+		d = Rsa.modinv(e, phi)
 		n = p*q
 		decrypted_message = pow(encrypted_message, d, n)
 		return hex(decrypted_message)[2:].split('L')[0]
@@ -80,6 +68,6 @@ class Rsa:
 	def modinv(a, m):
 		g, x, y = Rsa.egcd(a, m)
 		if g != 1:
-			raise Exception("Modular Inverse does not exist")
+			raise ModularInverseDoesNotExist(g, a, m)
 		else:
 			return x % m
